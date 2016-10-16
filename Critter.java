27780b133556a86086f1982006d139a7ca003155
ginalu <ginalu@utexas.edu>
2016-10-16 13:28:49 -0500

    moved query added

--- Critter.java
+++ Critter.java
@@ -23,6 +23,7 @@ public abstract class Critter {
 	private static String myPackage;
 	private	static List<Critter> population = new java.util.ArrayList<Critter>();
 	private static List<Critter> babies = new java.util.ArrayList<Critter>();
+	static List<Integer> moved = new java.util.ArrayList<Integer>();
 	
 	// Gets the package name.  This assumes that Critter and its subclasses are all in the same package.
 	static {
